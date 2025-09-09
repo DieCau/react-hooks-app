c641e0704c1a34238463d315cee4e0427760882d
Diego Caucota <diegocaucota@gmail.com>
2025-09-08 22:44:23 -0300

    add: useRef setfocus

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,9 @@ import { createRoot } from 'react-dom/client'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
+// import { PokemonPage } from './03-examples/PokemonPage';
 
-import { PokemonPage } from './03-examples/PokemonPage';
+import { FocusScreen } from './04-useRef/FocusScreen';
 
 import './index.css';
 
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
-    <PokemonPage />
+    {/* <PokemonPage /> */}
+    <FocusScreen />
   </StrictMode>,
 )
