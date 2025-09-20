c2971ea094a8271757a5b3e22a5c8c7aa8c72905
Diego Caucota <diegocaucota@gmail.com>
2025-09-20 00:43:59 -0300

    add: Zod validators implementation

--- src/05-useReducer/tasksReducer.ts
+++ src/05-useReducer/tasksReducer.ts
@@ -1,3 +1,5 @@
+import * as z from 'zod';
+
 interface Todo {
   id: number;
   text: string;
@@ -16,9 +18,23 @@ export type TaskAction =
   | { type: 'TOGGLE_TODO'; payload: number }
   | { type: 'DELETE_TODO'; payload: number };
 
+// Crear schema validator en Zod
+const TodoSchema = z.object({
+  id: z.number(),
+  text: z.string(),
+  completed: z.boolean(),
+});
+
+const TaskStateScheme = z.object({
+  todos: z.array(TodoSchema),
+  length: z.number(),
+  completed: z.number(),
+  pending: z.number(),
+});
+
 // Creo una funcion para configurar el ESTADO INICIAL
 export const getTaskInitialState = (): TaskState => {
-  const localStorageState = localStorage.getItem('task-state');
+  const localStorageState = localStorage.getItem('tasks-state');
 
   if (!localStorageState) {
     return {
@@ -29,7 +45,20 @@ export const getTaskInitialState = (): TaskState => {
     };
   }
 
-  return JSON.parse(localStorageState);
+  // Validar mediante Zod
+  const result = TaskStateScheme.safeParse(JSON.parse(localStorageState));
+
+  if (result.error) {
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0,
+    };
+  }
+
+  // Si no hay errores entonces colocamos el result.data
+  return result.data;
 };
 
 // un Reducer es una función que SIEMPRE regresa un valor o un estado nuevo
